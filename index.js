78d80fd349f06f6d7937bd5403a3ac983d972b55
Indicaza <indicazas@gmail.com>
2022-11-14 15:57:56 -0500

    Animating SVG stroke.

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-const infinity = document.querySelector('.infinity');
+// const infinity = document.querySelector('.infinity');
+//
+// const infinityLoader = new Moveit(infinity, {
+//     start: '0%',
+//     end: '10%'
+// });
+//
+// function load() {
+//     infinityLoader.set({
+//         start: "70%",
+//         end: "125",
+//         duration: 1.5,
+//         follow: true,
+//         callback: function() {
+//             load();
+//         },
+//     });
+// }
+// load();
 
-const infinityLoader = new Moveit(infinity, {
-    start: '0%',
-    end: '10%'
-});
-
-function load() {
-    infinityLoader.set({
-        start: "70%",
-        end: "125",
-        duration: 1.5,
-        follow: true,
-        callback: function() {
-            load();
-        },
-    });
-}
-load();
+var path = document.querySelector('.path');
+var length = path.getTotalLength();
