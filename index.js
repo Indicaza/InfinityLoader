ea39b82ffb9073b8d5af1c2a1024729b4e0b0833
Indicaza <indicazas@gmail.com>
2022-11-14 13:30:00 -0500

    Initial Commit.

---
+++ index.js
@@ -0,0 +1,19 @@
+const infinity = document.querySelector('.infinity');
+
+const infinityLoader = new Moveit(infinity, {
+    start: '0%',
+    end: '10%'
+});
+
+function load() {
+    infinityLoader.set({
+        start: "70%",
+        end: "125",
+        duration: 1.5,
+        follow: true,
+        callback: function() {
+            load();
+        },
+    });
+}
+load();
